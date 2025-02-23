bf0ff63895951900bf031bff2dd68eaf0d684a0b
Danny_Golds <danchik23122004@gmail.com>
2025-02-23 19:15:58 +0300

    icon -> trouble shooting

--- Security/resource.h
+++ Security/resource.h
@@ -9,7 +9,7 @@
 #define IDR_MAINFRAME                   128
 #define IDR_MENU1                       130
 #define IDD_DIALOG1                     131
-#define IDI_ICON1                       137
+#define IDI_ICON1                       138
 #define IDC_BUTTON1                     1000
 #define IDC_PROGRESS1                   1001
 #define IDC_EDIT1                       1002
@@ -36,7 +36,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        139
 #define _APS_NEXT_COMMAND_VALUE         32778
 #define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
